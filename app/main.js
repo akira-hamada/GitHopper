4111cc07991304438379a74a628352a069e9ad40
akira hamada <drumcorps.enthusiast@gmail.com>
2015-10-21 23:00:38 +0900

    表記を短縮形に統一

--- app/main.js
+++ app/main.js
@@ -38,7 +38,7 @@ var template = [
   {
     label: 'GitHub Viewer',
     submenu: [
-      { label: 'Preferences', accelerator: 'Command+,', click: function () {
+      { label: 'Preferences', accelerator: 'Cmd+,', click: function () {
           // ブラウザ(Chromium)の起動, 初期画面のロード
           prefWindow = new BrowserWindow({width: 500, height: 400, resizable: false});
           prefWindow.loadUrl('file://' + __dirname + '/preference.html');
@@ -46,7 +46,7 @@ var template = [
         }
       },
       { type: 'separator' },
-      { label: 'Quit GitHub Viewer', accelerator: 'Command+Q', click: function () { app.quit(); } },
+      { label: 'Quit GitHub Viewer', accelerator: 'Cmd+Q', click: function () { app.quit(); } },
     ]
   },
   {
@@ -61,37 +61,37 @@ var template = [
   {
     label: 'View',
     submenu: [
-      { label: 'Close', accelerator: 'Command+W', click: function() { BrowserWindow.getFocusedWindow().close(); } },
-      { label: 'Reload App', accelerator: 'Shift+Command+R', click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); } },
-      { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } },
-      { label: 'Toggle Sidebar', accelerator: 'CommandOrControl+S', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+s'); } },
+      { label: 'Close', accelerator: 'Cmd+W', click: function() { BrowserWindow.getFocusedWindow().close(); } },
+      { label: 'Reload App', accelerator: 'Shift+Cmd+R', click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); } },
+      { label: 'Toggle DevTools', accelerator: 'Alt+Cmd+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } },
+      { label: 'Toggle Sidebar', accelerator: 'CmdOrCtrl+S', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+s'); } },
       { type: 'separator' },
-      { label: 'Previous Repository', accelerator: 'CommandOrControl+K', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+k'); } },
-      { label: 'Next Repository', accelerator: 'CommandOrControl+J', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+j'); } },
-      { label: 'First Repository', accelerator: 'CommandOrControl+1', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+1'); } },
-      { label: 'Last Repository', accelerator: 'CommandOrControl+9', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+9'); } },
+      { label: 'Previous Repository', accelerator: 'CmdOrCtrl+K', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+k'); } },
+      { label: 'Next Repository', accelerator: 'CmdOrCtrl+J', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+j'); } },
+      { label: 'First Repository', accelerator: 'CmdOrCtrl+1', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+1'); } },
+      { label: 'Last Repository', accelerator: 'CmdOrCtrl+9', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+9'); } },
       { type: 'separator' },
-      { label: 'Keyboard Shortcuts', accelerator: 'CommandOrControl+/', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+/'); } },
+      { label: 'Keyboard Shortcuts', accelerator: 'CmdOrCtrl+/', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+/'); } },
     ]
   },
   {
     label: 'History',
     submenu: [
-      { label: 'Back', accelerator: 'CommandOrControl+[', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+['); } },
-      { label: 'Forward', accelerator: 'CommandOrControl+]', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+]'); } },
-      { label: 'Reload', accelerator: 'CommandOrControl+R', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+r'); } },
+      { label: 'Back', accelerator: 'CmdOrCtrl+[', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+['); } },
+      { label: 'Forward', accelerator: 'CmdOrCtrl+]', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+]'); } },
+      { label: 'Reload', accelerator: 'CmdOrCtrl+R', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+r'); } },
       { type: 'separator' },
-      { label: 'Copy Current Page URL', accelerator: 'CommandOrControl+U', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+u'); } },
-      { label: 'Open In Browser', accelerator: 'CommandOrControl+O', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+o'); } },
+      { label: 'Copy Current Page URL', accelerator: 'CmdOrCtrl+U', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+u'); } },
+      { label: 'Open In Browser', accelerator: 'CmdOrCtrl+O', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+o'); } },
     ]
   },
   {
     label: 'Repository',
     submenu: [
-      { label: 'PR/Issue Search', accelerator: 'CommandOrControl+F', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+f'); } },
-      { label: 'Pull Requests',   accelerator: 'CommandOrControl+P', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+p'); } },
-      { label: 'Issues',          accelerator: 'CommandOrControl+I', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+i'); } },
-      { label: 'Repository Top',  accelerator: 'CommandOrControl+T', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+t'); } },
+      { label: 'PR/Issue Search', accelerator: 'CmdOrCtrl+F', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+f'); } },
+      { label: 'Pull Requests',   accelerator: 'CmdOrCtrl+P', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+p'); } },
+      { label: 'Issues',          accelerator: 'CmdOrCtrl+I', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+i'); } },
+      { label: 'Repository Top',  accelerator: 'CmdOrCtrl+T', click: function() { mainWindow.webContents.send('onShortcutTriggered', 'Cmd+t'); } },
     ]
   },
 ];
