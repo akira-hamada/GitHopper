823ef7629e7f952a2eb17a7257077557056cd3c6
akira hamada <drumcorps.enthusiast@gmail.com>
2015-10-31 15:46:16 +0900

    不要なコメントを削除

--- app/main.js
+++ app/main.js
@@ -10,7 +10,6 @@ var Menu = require('menu');
 
 require('crash-reporter').start();
 
-// https://github.com/atom/electron/blob/master/docs/api/web-view-tag.md#event-new-window
 var mainWindow = null;
 var prefWindow = null;
 
